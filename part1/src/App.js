baa99dd8c4cc317ba415af46a6580be74d0bfcbb
Marley Alford <marleycalford@gmail.com>
2021-05-01 14:59:47 -0400

    Exercise 1.1

---
+++ part1/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Header = (props) => {  // renders name of course
+  return(<h1>{props.course}</h1>);
+
+}
+const Content = (props) => { // renders the parts and number of exercises
+  let content = [];
+  for (let i = 0; i < props.parts.length; i++) {
+    content.push(<p key={i}>{props.parts[i]} {props.exercises[i]}</p>);
+  }
+  return(<>{content}</>);
+
+}
+const Total = (props) => { // renders total number of exercises
+  return(<p>Number of exercises {props.total}</p>);
+
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
+      <Total total={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
