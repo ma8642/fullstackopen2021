799e0ecdf0905e3bbd4e6cb66d70b10446e61478
Marley Alford <marleycalford@gmail.com>
2021-05-03 22:55:03 -0400

    Exercise 2.1 + adding readmes for each section

---
+++ part2/README.md
@@ -0,0 +1,1 @@
+My submissions for [part2](https://fullstackopen.com/en/part2/rendering_a_collection_modules#rendering-collections)
---
+++ part2/courseinfo-ii/src/components/Content.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Part from "./Part";
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </>
+  );
+};
+export default Content;
